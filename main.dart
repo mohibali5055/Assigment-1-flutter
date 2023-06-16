ab924374fc50d7171820367eebba09aa3152e963
mohibali5055 <mohibali29j@gmail.com>
2023-06-16 14:56:01 +0500

    assigment_1_flutter_SMIT

---
+++ main.dart
@@ -0,0 +1,237 @@
+import 'dart:math';
+
+void main() {
+  
+  //Question no 1
+  
+//   int length = 4;
+//   int breadth = 4;
+  
+//   if(length == breadth){
+//     print('this is square');
+//   }else{
+//     print('this is rectangle');
+//   }
+  
+  //Q no 2
+  
+//   var young = 18;
+//   var old = 31;
+  
+//   if (young<old){
+//     print('this is young man');
+//   }
+//   else{
+//     print('this is oldest man');
+//   }
+  
+  //Q no 3
+  
+//   int classesOfheld = 16;
+//   int classesOfattended = 12;
+  
+//   num result = (classesOfattended/classesOfheld)*100;
+  
+  
+//   if (result>=75.00){
+//     print('you are allowed in exam');
+//   }else{
+//     print('you are not allowed in exam');
+//   }
+  
+  //Q no 4
+  
+//   int year = 1950;
+  
+//   if(year % 4 == 0){
+//     if(year % 100 == 0){
+//       if(year % 400 == 0){
+//         print('$year is leap year');
+//       }else{
+//         print('$year is not leap year');
+//       }
+//     }else{
+//       print('$year is leap year');
+//     }
+//   }else{
+//     print('$year is not leap year');
+//   }
+  
+  //Question no 5
+  
+ 
+//  int temp = 78;
+
+// if (temp < 0) {
+//   print('freezing weather');
+// } else if (temp <= 10) {
+//   print('very cold weather');
+// } else if (temp <= 20) {
+//   print('cold weather');
+// } else if (temp <= 30) {
+//   print('normal weather');
+// } else if (temp <= 40) {
+//   print('it\'s hot weather');
+// } else {
+//   print('it\'s very hot weather');
+// }
+  
+  
+  //Question no 6
+  
+//   String alpha = 'd';
+  
+//   String vowel = 'this is vowel letter';
+  
+//   if(alpha == 'a'){
+//     print(vowel);
+//   }else if(alpha == 'e'){
+//     print(vowel);
+//   }else if(alpha == 'i'){
+//     print(vowel);
+//   }else if(alpha == 'o'){
+//     print(vowel);
+//   }else if(alpha == 'u'){
+//     print(vowel);
+//   }else{
+//     print('this is consonant letter');
+//   }
+  
+  
+  //Question no 7
+  
+//   int id = 1991;
+  
+//   String name = 'mohib';
+  
+//   int units = 546;
+
+//   num charges;
+ 
+    
+//   if (units <= 199) {
+//     charges = 1.20;
+//   } else if (units >= 200 && units < 400) {
+//     charges = 1.50;
+//   } else if (units >= 400 && units < 600) {
+//     charges = 1.80;
+//   } else {
+//     charges = 2.00;
+//   }
+  
+//   num billAmount = units * charges;
+  
+//   print('Customer IDNO: $id');
+//   print('Customer Name: $name');
+//   print('Unit Consumed: $units');
+//   print('Amount charges: @Rs.$charges per unit');
+//   print('Net Bill Amount: $billAmount');
+  
+  
+  //Question no 8
+  
+//   String studentName = 'mohib';
+//   int studentRollnumber = 1001;
+//   int studentClass = 10;
+//   String studentGrade;
+  
+//   int subjectUrdu = 77;
+//   int subjectEnglish = 77;
+//   int subjectMath = 77;
+//   int subjectIslamiat = 77;
+//   int subjectComputer = 77;
+ 
+  
+  
+//   var allSubject = subjectUrdu+subjectEnglish+subjectMath+subjectIslamiat+subjectComputer;
+//   var result = (allSubject/600)*100;
+  
+//   if(result >= 50 && result < 60){
+//     studentGrade = 'D';
+//   }else if(result >= 60 && result < 70){
+//     studentGrade = 'C';
+//   }else if(result >= 70 && result < 80){
+//     studentGrade = 'B';
+//   }else if(result >= 80 && result < 90){
+//     studentGrade = 'A';
+//   }else if(result >= 90 && result <= 100){
+//     studentGrade = 'A+';
+//   }else{
+//     studentGrade = 'Fail';
+//   }
+  
+//   double percentage = result;
+  
+//   double roundOff = double.parse(percentage.toStringAsFixed(2));
+  
+  
+//   print('Student Name: $studentName');
+//    print('Student Roll Number: $studentRollnumber');
+//    print('Student Class: $studentClass');
+//    print('Student Percentage: $roundOff');
+//   print('Student Grade: $studentGrade');
+  
+  
+  //Question no 9
+  
+//   num value = 23;
+  
+  
+//   if(value % 2 == 0){
+    
+//      print('this is even number');
+    
+//     if(value % 5 != 0){
+      
+//       print('this is even number but not dividible by 5');
+      
+//     }
+    
+   
+    
+//   }else{
+//     print('this is odd number');
+//       if(value % 7 != 0){
+//         print('this is odd number but not divisible by 7');
+//       }
+//   }
+  
+  //Question no 10
+  
+//   int number1 = 8343;
+//   int number2 = 23;
+  
+//   if(number1 > number2){
+//     print('$number1 is greater than $number2');
+//   }else if(number2 > number1){
+//     print('$number2 is greater than $number1');
+//   }else{
+//     print('both are equal');
+//   }
+  
+  //Question no 11
+  
+//   num number = 24;
+//   num root = sqrt(number);
+  
+//   double roundoff = double.parse(root.toStringAsFixed(2));
+  
+//   print('The root of $number is $roundoff');
+    
+  //Question no 12
+  
+//   num celsius = 4;
+//   num fahrenheit = (celsius * 9/5) + 32;
+  
+//   print('$celsius Celsius Degree in Fahrenheit is $fahrenheit');
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
